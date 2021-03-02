abbebf80269cea605b2c3866a737867f898a9345
Egor Suschenko <lalalayes2@gmail.com>
2021-03-03 00:00:32 +0300

    feat: some React fetures implemented into app

---
+++ test-project/src/Loader.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <div style = {{display: 'flex', justifyContent: 'center', margin: '0.5rem'}}><div className = "lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
--- test-project/src/Todo/TodoList.js
+++ test-project/src/Todo/TodoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import TodoItem from './TodoItem';
 
 const styles = {
@@ -6,18 +7,25 @@ const styles = {
     padding: 0,
     margin: 0,
     listStyle: 'none',
+    marginLeft: '1rem',
 
   }
 }
 
 function TodoList (props) {
+
   return (
     <ul style = { styles.ul }>
       { props.items.map( (todo, index) => {
-        return <TodoItem todo = { todo } key = { todo.id } index = { index } ></TodoItem>
+        return <TodoItem todo = { todo } key = { todo.id } index = { index } onChange = { props.onToggle } ></TodoItem>
       })}
     </ul>
   );
+}
+
+TodoList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onToggle: PropTypes.func.isRequired
 }
 
 export default TodoList;
