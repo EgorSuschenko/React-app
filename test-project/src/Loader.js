9c661b8e7c38eef14690bacd0c95f5f529da4189
Egor Suschenko <lalalayes2@gmail.com>
2021-03-05 14:43:45 +0300

    Updates

--- test-project/src/Loader.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default () => <div style = {{display: 'flex', justifyContent: 'center', margin: '0.5rem'}}><div className = "lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
